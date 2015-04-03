dd4c1add42cf7db6f80f2468ebaa27a61f0ae630
lloydzhou <lloydzhou@qq.com>
2015-04-03 09:40:50 +0800

    update code

--- lua_cbson.c
+++ lua_cbson.c
@@ -18,42 +18,132 @@
 /* ===== DECODING ===== */
 
 typedef struct {
+    uint32_t count;
+    bool keys;
+    uint32_t depth;
     lua_State *l;
-    bool isobj;
 } cbson_state;
 
 static bool cbson_visit_document (const bson_iter_t *iter, const char *key, const bson_t *v_document, void *data);
 static bool cbson_visit_array (const bson_iter_t *iter, const char *key, const bson_t *v_array, void *data);
 
+static bool cbson_visit_before (const bson_iter_t *iter, const char *key, void *data){
+    cbson_state *s = data;
+    s->count++;
+    if (s->keys)
+        lua_pushstring(s->l, key);
+
+    return false;
+}
+
+static bool cbson_visit_after (const bson_iter_t *iter, const char *key, void *data){
+    cbson_state *s = data;
+    if (s->keys)
+        lua_rawset(s->l, -3);
+    else
+        lua_rawseti(s->l, -2, s->count);
+
+    return false;
+}
+
 static bool cbson_visit_utf8 (const bson_iter_t *iter, const char *key, size_t v_utf8_len, const char *v_utf8, void *data){
-    lua_pushstring(data, key);
-    lua_pushlstring(data, v_utf8, v_utf8_len);
-    lua_rawset(data, -3);
+    cbson_state *s = data;
+    lua_pushlstring(s->l, v_utf8, v_utf8_len);
+    return false;
+}
+
+static bool cbson_visit_int32 (const bson_iter_t *iter, const char *key, int32_t v_int32, void *data){
+    cbson_state *s = data;
+    lua_pushnumber(s->l, v_int32);
+    return false;
+}
+
+static bool cbson_visit_int64 (const bson_iter_t *iter, const char *key, int64_t v_int64, void *data){
+    cbson_state *s = data;
+    lua_pushnumber(s->l, v_int64);
+    return false;
+}
+
+static bool cbson_visit_double (const bson_iter_t *iter, const char *key, double v_d, void *data){
+    cbson_state *s = data;
+    lua_pushnumber(s->l, v_d);
+    return false;
+}
+
+static bool cbson_visit_undefined (const bson_iter_t *iter, const char *key, void *data){
+    cbson_state *s = data;
+    lua_newtable(s->l);
+    lua_pushlstring(s->l, "$undefined", 10);
+    lua_pushboolean(s->l, true);
+    lua_rawset(s->l, -3);
+    return false;
+}
+
+static bool cbson_visit_null (const bson_iter_t *iter, const char *key, void *data){
+    cbson_state *s = data;
+    lua_pushnil(s->l);
+    return false;
+}
+
+static bool cbson_visit_bool (const bson_iter_t *iter, const char *key, bool v_bool, void *data){
+    cbson_state *s = data;
+    lua_pushboolean(s->l, v_bool);
+    return false;
+}
+
+static bool cbson_visit_oid (const bson_iter_t *iter, const char *key, const bson_oid_t *oid, void *data){
+    cbson_state *s = data;
+    char str[25];
+    bson_return_val_if_fail (oid, false);
+    bson_oid_to_string (oid, str);
+    lua_pushlstring(s->l, str, 24);
+    return false;
+}
+
+static bool cbson_visit_regex (const bson_iter_t *iter, const char *key, const  char *v_reg, const char *v_options, void *data){
+    cbson_state *s = data;
+    lua_newtable(s->l);
+    lua_pushlstring(s->l, "$regex", 6);
+    lua_pushstring(s->l, v_reg);
+    lua_rawset(s->l, -3);
+    lua_newtable(s->l);
+    lua_pushlstring(s->l, "$options", 8);
+    lua_pushstring(s->l, v_options);
+    lua_rawset(s->l, -3);
+    return false;
+}
+
+static bool cbson_visit_date_time (const bson_iter_t *iter, const char *key, int64_t msec_since_epoch, void *data){
+    cbson_state *s = data;
+    lua_newtable(s->l);
+    lua_pushlstring(s->l, "$date", 5);
+    lua_pushnumber(s->l, msec_since_epoch);
+    lua_rawset(s->l, -3);
     return false;
 }
 
 static const bson_visitor_t cbson_visitors = {
-       NULL, /* visit_before */
-       NULL, /* visit_after */
+       cbson_visit_before,
+       cbson_visit_after,
        NULL, /* visit_corrupt */
-       NULL, /* visit_double */
+       cbson_visit_double,
        cbson_visit_utf8,
        cbson_visit_document,
        cbson_visit_array,
        NULL, /* visit_binary */
-       NULL, /* visit_undefined */
-       NULL, /* visit_oid */
-       NULL, /* visit_bool */
-       NULL, /* visit_date_time */
-       NULL, /* visit_null */
-       NULL, /* visit_regex */
+       cbson_visit_undefined,
+       cbson_visit_oid,
+       cbson_visit_bool,
+       cbson_visit_date_time,
+       cbson_visit_null,
+       cbson_visit_regex,
        NULL, /* visit_dbpointer */
        NULL, /* visit_code */
        NULL, /* visit_symbol */
        NULL, /* visit_codewscope */
-       NULL, /* visit_int32 */
+       cbson_visit_int32,
        NULL, /* visit_timestamp */
-       NULL, /* visit_int64 */
+       cbson_visit_int64,
        NULL, /* visit_maxkey */
        NULL, /* visit_minkey */
 
@@ -61,23 +151,29 @@ static const bson_visitor_t cbson_visitors = {
 
 static bool cbson_visit_document (const bson_iter_t *iter, const char *key, const bson_t *v_document, void *data){
     bson_iter_t iter_new;
-    lua_pushstring(data, key);
-    lua_newtable(data);
+
+    cbson_state *s = data;
+    lua_newtable(s->l);
+    cbson_state cs = {0, true};
     if (bson_iter_init(&iter_new, v_document)){
-        bson_iter_visit_all(&iter_new, &cbson_visitors, data);
+        cs.depth = s->depth + 1;
+        cs.l = s->l;
+        bson_iter_visit_all(&iter_new, &cbson_visitors, &cs);
     }
-    lua_rawset(data, -3);
     return false;
 }
 
 static bool cbson_visit_array (const bson_iter_t *iter, const char *key, const bson_t *v_array, void *data){
     bson_iter_t iter_new;
-    lua_pushstring(data, key);
-    lua_newtable(data);
+
+    cbson_state *s = data;
+    lua_newtable(s->l);
+    cbson_state cs = {0, false};
     if (bson_iter_init(&iter_new, v_array)){
-        bson_iter_visit_all(&iter_new, &cbson_visitors, data);
+        cs.depth = s->depth + 1;
+        cs.l = s->l;
+        bson_iter_visit_all(&iter_new, &cbson_visitors, &cs);
     }
-    lua_rawset(data, -3);
     return false;
 }
 
@@ -85,15 +181,14 @@ static bool cbson_visit_array (const bson_iter_t *iter, const char *key, const b
 static int bson_decode(lua_State *l)
 {
     bson_t *bson;
-    cbson_state s;
+    cbson_state s = {0, false};
     bson_iter_t iter;
     luaL_argcheck(l, lua_gettop(l) == 1, 1, "expected 1 argument");
     bson = lua_topointer(l, 1);
     s.l = l;
-    s.isobj = false;
 
     if (bson_iter_init(&iter, bson)){
-        bson_iter_visit_all(&iter, &cbson_visitors, s.l);
+        bson_iter_visit_all(&iter, &cbson_visitors, &s);
     }
 
     return 1;
--- test.lua
+++ test.lua
@@ -10,7 +10,9 @@ print(bson._VERSION)
 local b = mongo.bson('{"hello": "world"}')
 print (b.to_json())
 
-print(bson.decode(mongo.bson('{"arr": {"bbb": "aaa"}}')))
+print(bson.decode(mongo.bson('{"arr": {"bbb": "aaa", "ccc": 1, "ddd": 2.1, "undef": {"$aaa": "$sdf"}}}')))
+print(mongo.bson('{"arr": [{"bbb": 1}, {"aaa": 2.1}]}'))
+print(bson.decode(mongo.bson('{"arr": ["bbb", "aaa", 1, 2,1, null, true, {"$oid": "54a3ef5369401b30e2cd0b98"}, {"$date": 1339582446756}]}')))
 print(bson.decode(mongo.bson('{"hello": "world"}')))
 
 --print(json.encode(bson.decode(mongo.bson())))
